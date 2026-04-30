d2a67cd7f6fb54cfee611338dc42d3cca1e272fd
santhiya R <santhiyaravi58@gmail.com>
2026-04-30 22:55:11 +0530

    Time: 10 ms (99.68%) | Memory: 98.2 MB (6.24%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null || nums.length==0)
+            return false;
+        HashSet<Integer> hset = new HashSet<Integer>();
+        for(int idx: nums){
+            if(!hset.add(idx)){
+                return true;
+            }
+        }
+      
+        return false;
+    }
+}
